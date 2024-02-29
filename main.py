eab230e58192c0fa8700d73a828228a7743c12ac
teacher <arturshkolnikov@gmail.com>
2024-02-29 12:18:38 +0300

    One_1

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ def f(s, m):
     if s >= 301: return m % 2 == 0
     if m == 0: return 0
     h = [f(s + 3, m - 1), f(s * 5, m - 1)]
-    return any(h) if (m - 1) % 2 == 0 else all(h) # any(h) для 19й задачи т.к. неуд ход
+
 
